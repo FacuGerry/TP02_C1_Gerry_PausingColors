937a32f88784dd45198b72896f02cd56e9ef5e4f
Facundo Gerry <facuto365pro@gmail.com>
2025-08-20 17:05:34 -0300

    no errors any more!!!

--- Assets/Scripts/Panels.cs
+++ Assets/Scripts/Panels.cs
@@ -1,8 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class Panels : MonoBehaviour
