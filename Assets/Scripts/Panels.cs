3f539f98c59bf18275a82ce8d439887e1427441a
Facundo Gerry <facuto365pro@gmail.com>
2025-08-20 10:23:36 -0300

    UI almost done
    La UI esta hecha pero me falta que el boton de exit del PanelPause cierre el juego. Tambien falta saber pq tira error el bton de exit en los creditos y en las options xd

---
+++ Assets/Scripts/Panels.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Panels : MonoBehaviour
+{
+
+    [Header("Panel Settings")]
+    [SerializeField] private GameObject panelPause;
+    [SerializeField] private GameObject panelOptions;
+    [SerializeField] private GameObject panelCredits;
+
+    [Header("Button Settings")]
+    [SerializeField] private Button btnPlay;
+    [SerializeField] private Button btnOptions;
+    [SerializeField] private Button btnCredits;
+    [SerializeField] private Button btnExit;
+    [SerializeField] private Button btnBackOptions;
+    [SerializeField] private Button btnBackCredits;
+
+    [Header("Slider Settings")]
+    [SerializeField] private Slider speedPlayer1;
+    [SerializeField] private Slider speedPlayer2;
+
+    private bool isPause = false;
+
+    void Awake()
+    {
+        btnPlay.onClick.AddListener(Resume);
+        btnOptions.onClick.AddListener(OnOptionsClicked);
+        btnCredits.onClick.AddListener(OnCreditsClicked);
+        btnExit.onClick.AddListener(OnExitClicked);
+        btnBackOptions.onClick.AddListener(OnBackOptionsClicked);
+        btnBackCredits.onClick.AddListener(OnBackCreditsClicked);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!isPause)
+            {
+                Pause();
+            }
+            else
+            {
+                Resume();
+            }
+        }
+    }
+
+    void Pause()
+    {
+        Time.timeScale = 0;
+        isPause = true;
+        panelPause.SetActive(true);
+    }
+
+    void Resume()
+    {
+        Time.timeScale = 1;
+        isPause = false;
+        panelPause.SetActive(false);
+    }
+
+    void OnOptionsClicked()
+    {
+        panelPause.SetActive(false);
+        panelOptions.SetActive(true);
+    }
+
+    void OnCreditsClicked()
+    {
+        panelPause.SetActive(false);
+        panelCredits.SetActive(true);
+    }
+
+    void OnExitClicked()
+    {
+        Application.Quit();
+    }
+
+    void OnBackOptionsClicked()
+    {
+            panelOptions.SetActive(false);
+            panelPause.SetActive(true);
+    }
+
+    void OnBackCreditsClicked()
+    {
+            panelCredits.SetActive(false);
+            panelPause.SetActive(true);
+    }
+
+}
