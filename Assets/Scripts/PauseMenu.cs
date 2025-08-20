5df12b7055a930ba77af75f2fd077d7189188058
Facundo Gerry <facuto365pro@gmail.com>
2025-08-19 21:49:55 -0300

    Pause script added

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -4,14 +4,37 @@ using UnityEngine;
 
 public class PauseMenu : MonoBehaviour
 {
-    
+    bool isPause = true;
     void Awake()
     {
-        
+
     }
 
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.K))
+        {
+            if (isPause)
+            {
+                Pause();
+            }
+            else
+            {
+                Resume();
+            }
+        }
     }
+
+
+
+    void Pause()
+    {
+        Time.timeScale = 0;
+    }
+
+    void Resume()
+    {
+        Time.timeScale = 1;
+    }
+
 }
